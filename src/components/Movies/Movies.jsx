68410ed15ef18d265a6e2a04121147d22041fe10
Olga Kalyazhinkova <oi.kalyazhinkova@yandex.ru>
2023-02-04 00:21:01 +0300

    Бесконечность

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchForm from './SearchForm/SearchForm';
 import MoviesCardList from './MoviesCardList/MoviesCardList';
 import moviesApi from '../../utils/MoviesApi';
 import mainApi from '../../utils/MainApi';
 
 export default function Movies({ loggedIn }) {
-  if (loggedIn) {
-    const films = moviesApi.getAllMovies();
-    console.log(films);
-  }
+  const [movies, setMovies] = useState([null]);
+
+      moviesApi.getAllMovies()
+        .then((allMovies) => {
+          setMovies(allMovies.slice());
+          console.log('then');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+    console.log(movies);
+
 
   return (
     <main className="main">
